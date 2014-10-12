a754fb7c9f0b30f52aea8376a9670a7e67cffd3a
Kevin DeLoach <kdeloach@gmail.com>
2014-10-11 21:24:42 -0400

    Try other layout strategies

--- static/js/app/layouts/flow.js
+++ static/js/app/layouts/flow.js
@@ -6,24 +6,26 @@ define(['lodash', 'jquery', 'jquery-ui', '../class', '../cardxy'],
         // Where would each card go in this region?
         calculate: function(region, cardEls) {
             // Assumes all cards have the same dimensions.
-            var c = _.first(cardEls),
+            var firstCard = _.first(cardEls),
                 regionWidth = region.$el.width(),
                 regionHeight = region.$el.height(),
-                cardWidth = c.width(),
-                cardHeight = c.height();
-                
-            var scale = 1,
-                scaleDenom = 0;
-            while (!willFit(regionWidth,
-                            regionHeight,
-                            cardWidth * scale,
-                            cardHeight * scale,
-                            cardEls.length)) {
-                scale = 1 - ++scaleDenom * 0.01;
-            }
+                cardWidth = firstCard.width(),
+                cardHeight = firstCard.height();
+
+            var scale = scaleDenomStrategy({
+                regionWidth: regionWidth,
+                regionHeight: regionHeight,
+                cardWidth: cardWidth,
+                cardHeight: cardHeight,
+                numCards: cardEls.length
+            });
 
             var cols = maxCols(regionWidth, cardWidth * scale),
-                getPointAt = indexToPoint(cols, cardWidth * scale, cardHeight * scale);
+                getPointAt = indexToPoint(
+                    cols,
+                    cardWidth * scale,
+                    cardHeight * scale
+                );
 
             return _.map(cardEls, function(cardEl, i) {
                 var point = getPointAt(i);
@@ -36,8 +38,68 @@ define(['lodash', 'jquery', 'jquery-ui', '../class', '../cardxy'],
         }
     });
 
-    // Will `n` amount of cards fit inside region?
+    // Reduce scale by 1% until all cards fit inside region.
+    // This works reasonably well.
+    function scaleDenomStrategy(params) {
+        var scale = 1,
+            scaleDenom = 0;
+        while (!willFit(params.regionWidth,
+                        params.regionHeight,
+                        params.cardWidth * scale,
+                        params.cardHeight * scale,
+                        params.numCards)) {
+             console.log(1);
+            scale = 1 - ++scaleDenom * 0.01;
+        }
+        return scale;
+    }
+
+    // Fit as many cards as possible horizontally and then increase width
+    // until we run out of vertical space.
+    // This works terribly.
+    function fitWidthStrategy(params) {
+        var cardsPerRow = params.numCards,
+            scaledCardWidth = params.regionWidth / cardsPerRow,
+            scale = scaledCardWidth / params.cardWidth;
+        while (willFit(params.regionWidth,
+                       params.regionHeight,
+                       params.cardWidth * scale,
+                       params.cardHeight * scale,
+                       params.numCards)) {
+            cardsPerRow -= 1;
+            scaledCardWidth = params.regionWidth / cardsPerRow;
+            scale = scaledCardWidth / params.cardWidth;
+        }
+        cardsPerRow += 1;
+        scaledCardWidth = params.regionWidth / cardsPerRow;
+        scale = scaledCardWidth / params.cardWidth;
+        return scale;
+    }
+
+    // Fit all cards horizontally and keep increasing by 10px until we
+    // run out of vertical space.
+    function growCardsStrategy(params) {
+        var scaledCardWidth = params.regionWidth / params.numCards,
+            scale = scaledCardWidth / params.cardWidth;
+        while (willFit(params.regionWidth,
+                       params.regionHeight,
+                       params.cardWidth * scale,
+                       params.cardHeight * scale,
+                       params.numCards)) {
+            scaledCardWidth += 1;
+            scale = scaledCardWidth / params.cardWidth;
+        }
+        return scale;
+    }
+
     function willFit(regionWidth, regionHeight, cardWidth, cardHeight, n) {
+        var cols = maxCols(regionWidth, cardWidth),
+            rows = maxRows(regionHeight, cardHeight);
+        return n < cols * rows;
+    }
+
+    // Will `n` amount of cards fit inside region?
+    function willFit_old(regionWidth, regionHeight, cardWidth, cardHeight, n) {
         var cols = maxCols(regionWidth, cardWidth),
             getPointAt = indexToPoint(cols, cardWidth, cardHeight),
             point = getPointAt(n - 1);
@@ -49,6 +111,10 @@ define(['lodash', 'jquery', 'jquery-ui', '../class', '../cardxy'],
     // TODO: Throw layout exception when cards won't fit.
     function maxCols(regionWidth, cardWidth) {
         return Math.max(1, Math.floor(regionWidth / cardWidth));
+    }
+
+    function maxRows(regionHeight, cardHeight) {
+        return Math.max(1, Math.floor(regionHeight / cardHeight));
     }
 
     // Return X & Y for given index.
